3ea141ccc11d1869bdac0a373bfc976a2b25688f
Nikos Zabakikas <100320490+CipherSh1nj0uu@users.noreply.github.com>
2024-09-25 15:13:28 +0300

    initializing main

---
+++ main.sh
@@ -0,0 +1,6 @@
+
+main() {
+echo -e "\nWelcome to my very first...primitive toolbox! You can use this set of tools however you want!\n\nChoose the tool you want to use.\nPassword Generator(1) - AutoFilter(2)\n"
+}
+
+main
