13667bdddc8e766bdab0c7af9c7b7c6bdd1f60a7
Nikos Zabakikas <100320490+CipherSh1nj0uu@users.noreply.github.com>
2024-09-25 15:33:36 +0300

    more initializing

--- main.sh
+++ main.sh
@@ -1,6 +1,14 @@
+source ./tools/passgener
+source ./tools/autofilter
 
 main() {
 echo -e "\nWelcome to my very first...primitive toolbox! You can use this set of tools however you want!\n\nChoose the tool you want to use.\nPassword Generator(1) - AutoFilter(2)\n"
+
+read -p "Enter a number: " tool
+if [[tool == 1]]; then
+	
+elif [[tool == 2]]; then
+
 }
 
 main
